1eb9c0c090a8aabb5b199d6b71b10e5a32570de6
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-07-26 03:25:51 +0900

    foi colocado a função de diminuir

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,9 +1,16 @@
 // This is a JavaScript file
 
-$(document).on("click","#somar", function(){
-  var primeiroValor = $("#valor1").val();
-  var segundoValor = $("#valor2").val();
-  var resultadoSoma = parseInt(primeiroValor) + parseInt(segundoValor);
 
+$(document).on("click","#somar", function(){
+  primeiroValor = $("#valor1").val();
+  segundoValor = $("#valor2").val();
+  var resultadoSoma = parseInt(primeiroValor) + parseInt(segundoValor);
   $("#resultado").val(resultadoSoma);
+})
+
+$(document).on("click","#diminuir", function(){
+  primeiroValor = $("#valor1").val();
+  segundoValor = $("#valor2").val();
+  var resultadoDiminuir = primeiroValor - segundoValor;
+  $("#resultado").val(resultadoDiminuir);
 })
