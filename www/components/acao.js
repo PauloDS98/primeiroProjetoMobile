aa248e228f4727929489d66188ef1437d9f8f5e2
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-07-26 03:34:38 +0900

    arrumado função dos valores no JS

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,23 +1,26 @@
 // This is a JavaScript file
-
-
-$(document).on("click","#somar", function(){
+$(document).on('change',"#valor1", function(){
   primeiroValor = $("#valor1").val();
+})
+
+$(document).on('change',"#valor2", function(){
   segundoValor = $("#valor2").val();
+})
+
+//função somar
+$(document).on("click","#somar", function(){
   var resultadoSoma = parseInt(primeiroValor) + parseInt(segundoValor);
   $("#resultado").val(resultadoSoma);
 })
 
+//função diminuir
 $(document).on("click","#diminuir", function(){
-  primeiroValor = $("#valor1").val();
-  segundoValor = $("#valor2").val();
   var resultadoDiminuir = primeiroValor - segundoValor;
   $("#resultado").val(resultadoDiminuir);
 })
 
+//função multiplicar
 $(document).on("click","#multiplicar", function(){
-  primeiroValor = $("#valor1").val();
-  segundoValor = $("#valor2").val();
   var resultadoMultiplicar = primeiroValor * segundoValor;
   $("#resultado").val(resultadoMultiplicar);
 })
