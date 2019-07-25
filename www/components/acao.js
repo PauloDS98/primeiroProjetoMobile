07a53a9cb784cdddcb9d6ede58306588bcdffe0b
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-07-26 03:02:56 +0900

    foi inserido a função de somar no JS

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,9 @@
 // This is a JavaScript file
 
+$(document).on("click","#somar", function(){
+  var primeiroValor = $("#valor1").val();
+  var segundoValor = $("#valor2").val();
+  var resultadoSoma = parseInt(primeiroValor) + parseInt(segundoValor);
+
+  $("#resultado").val(resultadoSoma);
+})
